9fe0692f99bf427974a6b29a600289e2f23406b8
Gurcan Hamali <gurcanhamali@gmail.com>
2020-10-13 18:18:38 +1100

    53. Outputting a List of Recipes with ngFor

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Recipe } from '../recipes/recipe-model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -7,14 +6,6 @@ import { Recipe } from '../recipes/recipe-model';
   styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent implements OnInit {
-  recipes: Recipe[] = [
-    new Recipe(
-      'A Test Recipe',
-      'This is a test.',
-      'https://www.glutenfreeandmore.com/wp-content/uploads/2018/07/10brisket.jpg'
-    ),
-  ];
-
   constructor() {}
 
   ngOnInit() {}
